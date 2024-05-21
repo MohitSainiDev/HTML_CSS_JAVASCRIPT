5b628d50d4055cf0865b36c871bb346347e324e5
MohitSainiDev <mht4943@gmail.com>
2024-05-21 21:27:24 +0530

    starting javascript

---
+++ Javascript/app.js
@@ -0,0 +1,22 @@
+// alert("Hello world")
+// document.write("<em>hello world</em>")
+// document.write("<br>")
+// document.write("<h1 style='color:red;'>mohit saini</h1>")
+
+// document.getElementById("Website").innerHTML = "hehe"
+// document.getElementById("paragraph").innerHTML="something else"
+
+// document.getElementById("fruit_title").style.color="orange"
+// document.querySelector(".fruit").style.color="orange"
+// document.querySelector("#fruit_title").innerHTML="Sfsd"
+
+let x="hello"
+let z="Computer Science"
+let a=1
+let b=a+1
+let c=a+b
+
+let firstName="Mohit"
+let secondName="Saini"
+console.log(firstName)
+console.log(secondName)
---
+++ Javascript/using_variables/app.js
@@ -0,0 +1,6 @@
+// let my_textbox = document.querySelector("#my_textbox")
+// document.querySelector("#my_title").innerHTML="my_Website"
+
+let title=document.querySelector("#my_title")
+title.innerHTML="my_Website"
+title.style.color="red"
