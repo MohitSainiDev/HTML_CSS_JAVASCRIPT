83e083e31aae1187becec418a37514deb94a5434
MohitSainiDev <mht4943@gmail.com>
2024-06-01 21:27:17 +0530

    changed

--- Javascript/app.js
+++ Javascript/app.js
@@ -10,13 +10,14 @@
 // document.querySelector(".fruit").style.color="orange"
 // document.querySelector("#fruit_title").innerHTML="Sfsd"
 
-let x="hello"
-let z="Computer Science"
-let a=1
-let b=a+1
-let c=a+b
+let x = "hello";
+let z = "Computer Science";
+let a = 1;
+//let
+let b = a + 1;
+let c = a + b;
 
-let firstName="Mohit"
-let secondName="Saini"
-console.log(firstName)
-console.log(secondName)
+let firstName = "Mohit";
+let secondName = "Saini";
+console.log(firstName);
+console.log(secondName);
